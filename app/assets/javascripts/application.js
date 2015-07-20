69abdc3211fae2462ea742e67b51f0e9b789a4dc
Alex McRitchie <amcritchie@gmail.com>
2015-07-20 00:01:00 -0600

    refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,54 +16,16 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    setTimeout(function() {
-        $('.background-page').each(function(index, element) {
-//        debugger;
+//    setTimeout(function () {
+        $('.background-wrapper').each(function (index, element) {
             $(element).css('visibility', 'hidden');
-//            $(element).find('img').css('visibility', 'hidden');
-            $(element).find('img').on('load', function() {
-                        $(".page-load-spinner").fadeOut('medium');
+            $(element).find('img').on('load', function () {
+                $(".page-load-spinner").fadeOut('medium');
                 $(element).css('visibility', 'visible').hide().fadeIn('slow');
-
-                debugger;
             });
-//            $(element).data('background-image-path');
-//            $(element).find('img').attr('src', '/images/iron_ocean.jpeg');
             $(element).find('img').attr('src', $(element).data('background-image-path'));
-            debugger;
-//            $(element).find('img').css('src', '/images/iron_ocean.jpeg');
-//        $(element).find('img').onLoad(funtion() {
-//            debugger;
-//        });
-//        $('<img/>').attr('src', 'http://picture.de/image.png').load(function() {
-//            $(this).remove(); // prevent memory leaks as @benweet suggested
-//            $('body').css('background-image', 'url(http://picture.de/image.png)');
-//        });
         });
-    },1000);
-
-
-//    var img = new Image();
-//    $.when(minTime(), onImageLoad()).then(function () {
-//        $(".background-image").css("background-image", "url('" + img.src + "')").fadeIn('medium');
-//        $(".page-load-spinner").fadeOut('medium');
-//    });
-//    function minTime() {
-//        var minLoadTime = 500;
-//        var deferred = $.Deferred();
-//        setTimeout(function () {
-//            deferred.resolve();
-//        }, minLoadTime);
-//        return deferred;
-//    }
-//    function onImageLoad() {
-//        var deferred = $.Deferred();
-//        img.src = "http://farm1.staticflickr.com/293/18278124840_42bcd162cb_b.jpg";
-//        img.onload = function () {
-//            deferred.resolve();
-//        };
-//        return deferred;
-//    }
+//    }, 1000);
 });
