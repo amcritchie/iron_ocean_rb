201afca2e2467e9b082c75bdb882aa1a1069a83a
Alex McRitchie <amcritchie@gmail.com>
2015-08-23 00:30:49 -0600

    update to config files

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/blog/current"
+root = "/home/deployer/apps/iron_ocean/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.iron_ocean.sock"
 worker_processes 2
 timeout 30
